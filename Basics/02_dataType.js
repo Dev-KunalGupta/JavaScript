0a70f4d1874d2cf01240d8b13d9b8186fd717ca7
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-05-02 18:18:01 +0000

    Practice

---
+++ Basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict" //Treat JS code as new version
+
+// alert(2+3) Running on node.js not on browser
+
+/*
+    JS DATATYPES ---> Primitives
+number => 2 to the power 53
+bigint
+boolean
+string => ""
+null => standalone value
+undefined
+symbol => unique
+
+
+
+Object => non-primitive
+ */
+
+console.log(typeof(null))       //object
+console.log(typeof(undefined))  //undefined
