4524024d1be81c1657f3bf735c44852e73e5342a
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-05-02 15:55:24 +0000

    Deleted

---
+++ Basics/basic.js
@@ -0,0 +1,10 @@
+// let value = Math.floor(Math.random() * 10)+1
+// console.log(value)
+
+// console.log(typeof Date)
+
+// let date = new Date()
+// console.log(typeof(date));
+
+const str = 'kunal'
+console.log(str.length);
