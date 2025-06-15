3d024dd4d05f20d7466dcd5cc9eb54375f6e319c
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-06-15 07:21:45 +0000

    conversion of datatype

---
+++ Basics/03_typeConversion.js
@@ -0,0 +1,15 @@
+let score = '33abc'
+
+console.log(typeof score)
+console.log(score)
+
+const scoreInNumber = Number(score)
+
+console.log(typeof scoreInNumber)
+console.log(scoreInNumber)
+
+// '33' => 33
+// '33abc' => NaN
+// true => 1  false => 0
+// null => 0
+// undefined => NaN
