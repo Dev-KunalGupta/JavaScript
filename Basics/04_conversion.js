7ff5e80a3e882fd1393a6a91cea40ce492029209
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-06-16 13:47:58 +0000

    type conversion

---
+++ Basics/04_conversion.js
@@ -0,0 +1,17 @@
+console.log("2" == 2)   //implicit type conversion 
+console.log("2" === 2)  //checks datatype also
+
+
+//comparison and equality check work differently
+console.log(null > 0)   //converts null to a number i.e 0
+console.log(null < 0)   //converts null to a number i.e 0
+console.log(null >= 0)  //converts null to a number i.e 0
+console.log(null <= 0)  //converts null to a number i.e 0
+console.log(null == 0)
+
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+console.log(undefined == 0)
