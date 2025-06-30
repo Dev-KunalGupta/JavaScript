10517e77f10b2c7d763b5b937fcc51393b4e8976
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-06-30 18:25:01 +0000

    foreach loop

---
+++ 05_Loops.js/03.js
@@ -0,0 +1,43 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach(function name() {
+    // console.log("Kunal")
+})
+
+coding.forEach(function (item) {
+    // console.log(item)
+})
+
+coding.forEach( (item) => {
+    // console.log(item)
+})
+
+function printKeys(item){
+    console.log(item)
+}
+
+// coding.forEach(printKeys)
+
+
+coding.forEach((item, index, arr)=>{
+    // console.log(item, index, arr)
+})
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach((item, index, json)=>{
+    // console.log(item, index, json)
+})
