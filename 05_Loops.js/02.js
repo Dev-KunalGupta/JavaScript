133c7bb77e85d5f8c732a89c7e9d1c575eefd56d
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-06-30 18:05:51 +0000

    forin loop

--- 05_Loops.js/02.js
+++ 05_Loops.js/02.js
@@ -22,3 +22,28 @@ for (const key in obj) {
 for (const key in obj) {
     // console.log(key + ': ' + obj[key])   // print key: value pairs
 }
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    // console.log(key);           // Nothing is printed (forin not work on map)
+}
+
+// Let's check on arrays and string
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const key in arr) {
+    // console.log(key)                  // Print Indices
+}
+
+const str = "Kunal Gupta"
+
+for (const key in str) {
+    // console.log(key)                  // Print Indices
+}
