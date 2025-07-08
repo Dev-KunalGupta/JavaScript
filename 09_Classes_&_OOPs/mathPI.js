44eb2628fc648cc88082d6925e50bbc7e4fe8e19
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-07-08 17:31:21 +0000

    object descriptor

--- 09_Classes_&_OOPs/mathPI.js
+++ 09_Classes_&_OOPs/mathPI.js
@@ -1,6 +1,27 @@
 // console.log(Math.PI);
-// Math.PI = 4;              // Not change obviously
+// Math.PI = 4;                 // Not change obviously.
 // console.log(Math.PI);
 
-const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI');
-console.log(descriptor);
+// const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI');
+// console.log(descriptor);     // The reason.
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {enumerable: false})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (const [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function') 
+    console.log(key + ':' + value) // Name will not be printed(enumerable:false)
+}
