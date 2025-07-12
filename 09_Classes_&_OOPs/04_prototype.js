10083257284467f17179a6d37c590ed78e74c23f
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-07-12 12:38:22 +0000

    prototype

--- 09_Classes_&_OOPs/04_prototype.js
+++ 09_Classes_&_OOPs/04_prototype.js
@@ -7,3 +7,30 @@ String.prototype.trueLength = function(){
 }
 
 console.log(myName.trueLength());
+
+// Now do the same thing for Arrays and Objects.
+let myArray = ["thor","ironman", "spiderman"]
+
+
+let myObject = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Array.prototype.kunali = function(){
+    console.log('Kunal is present in all arrays.')
+}
+
+Object.prototype.kunal = function(){
+    console.log('Kunal is present in all objects.')
+}
+
+myArray.kunal();         // can access object wala function also
+myArray.kunali();
+
+myObject.getSpiderPower();  // but object can't access array wala function
+myObject.kunal();
