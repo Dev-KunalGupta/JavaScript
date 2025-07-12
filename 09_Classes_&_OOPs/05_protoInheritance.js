c6e62ed659de3951eb3d175aec26c0c6259467a1
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-07-12 12:48:52 +0000

    prototypal inheritance in js

---
+++ 09_Classes_&_OOPs/05_protoInheritance.js
@@ -0,0 +1,38 @@
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// console.log(Teacher.name)                    chai
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+// console.log(TeachingSupport.makeVideo)       true
+
+let anotherUsername = "ChaiAurCode     ";
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
