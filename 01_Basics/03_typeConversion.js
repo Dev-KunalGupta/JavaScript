2b9c9c127c03bcae719eaf8d68dfc0aea3730edc
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-07-16 09:11:41 +0000

    weird type conversion

--- 01_Basics/03_typeConversion.js
+++ 01_Basics/03_typeConversion.js
@@ -13,3 +13,14 @@ console.log(scoreInNumber)
 // true => 1  false => 0
 // null => 0
 // undefined => NaN
+
+
+// '1' == 1        // true   (string '1' is coerced to number 1)
+// 0 == false      // true   (false coerces to 0)
+// null == undefined // true (special case)
+
+
+// false == '0'        // true
+// '' == 0             // true
+// [] == false         // true
+// [] == ![]           // true 🤯
