8d68a67f1d77c5088ab9928d37332bbc4c10a718
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-07-16 17:54:01 +0000

    beautification

--- 09_Classes_&_OOPs/03_mathPI.js
+++ 09_Classes_&_OOPs/03_mathPI.js
@@ -1,5 +1,5 @@
 // console.log(Math.PI);
-// Math.PI = 4;                 // Not change obviously.
+// Math.PI = 4;                 // Not change obviously
 // console.log(Math.PI);
 
 // const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI');
